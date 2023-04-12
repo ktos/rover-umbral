62a87eaa2467873ee1ccf404ded8e033fb349eb1
Marcin Badurowicz <m@badurowicz.net>
2023-04-12 18:49:53 +0200

    Add Mokosh

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
+#include <Mokosh.hpp>
+
+Mokosh mokosh;
+
 void setup()
 {
+    mokosh.setDebugLevel(DebugLevel::DEBUG)
+        ->setForceWiFiReconnect(true)
+        ->setHeartbeat(false)
+        ->setMDNS(false);
+
+    mokosh.begin("UmbralRose");
 }
 
 void loop()
 {
+    mokosh.loop();
 }
